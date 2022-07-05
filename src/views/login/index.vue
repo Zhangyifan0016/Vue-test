8f26d6b68f877da0f8205bc7fd03cda18af4488c
Zhangyifan0016 <z351072259@163.com>
2022-07-05 10:22:54 +0800

    feat(src目录): 引入全局样式styles 下载并引入element-plus 配置登录路由

---
+++ src/views/login/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <el-form
+      ref="ruleFormRef"
+      :model="loginForm"
+      :rules="rules"
+      label-width="120px"
+      class="demo-ruleForm"
+    >
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="loginForm.username" />
+      </el-form-item>
+      <el-form-item label="密码" prop="username">
+        <el-input v-model="loginForm.password" type="password" />
+      </el-form-item>
+      <el-form-item label="验证码" prop="captcha">
+        <el-input v-model="loginForm.captcha" />
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script setup>
+import { reactive } from 'vue'
+const loginForm = reactive({
+  username: 'test',
+  password: '',
+  captcha: ''
+})
+</script>
+<style scoped lang="scss"></style>
